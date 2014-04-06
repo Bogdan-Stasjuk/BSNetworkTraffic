36d1463ee2b4613ab1adea8d1fca8c6497ca36e4
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-04-06 17:48:09 +0300

    Initial Commit

---
+++ NetworkTrafficTracking/NTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTTAppDelegate.h
+//  NetworkTrafficTracking
+//
+//  Created by Bogdan Stasjuk on 4/6/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkTrafficTracking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetworkTrafficTracking
+//
+//  Created by Bogdan Stasjuk on 4/6/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTTAppDelegate class]));
+    }
+}
